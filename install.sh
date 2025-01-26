3904915567eddd840f43ed61a52e71c392db1c3d
nathan-r-a-schultz <nathan.r.a.schultz@gmail.com>
2025-01-26 13:44:11 +0000

    Easier reinstallation process

--- install.sh
+++ install.sh
@@ -1,5 +1,61 @@
 #!/bin/bash
 
+# the environment config function
+configEnvVars() {
+
+    # get the user's API key and location
+    echo "Please enter your weatherapi.com API key (input is hidden):"
+    read -sp "API Key: " API_KEY
+    echo -e "\nThe following formats for locations are valid:\n[1] US Zipcode\n[2] Canada Postalcode\n[3] IP Address\n[4] Latitude/Longitude (decimal degree)\n[5] City name (not recommended for small towns or cities with common names)\n"
+    read -p "Enter your option [1,2,3,4,5]: " FORMAT_SELECTION
+
+    # Ask the user for their location based on their selection
+    case "$FORMAT_SELECTION" in
+
+        1)
+            echo "Option [1] selected"
+            read -p "Enter your US Zipcode: " LOCATION
+        ;;
+
+        2)
+            echo "Option [2] selected"
+            read -p "Enter your Canada Postalcode (do not use spaces): " LOCATION
+        ;;
+
+        3)
+            echo "Option [3] selected"
+            echo "Available IP addresses: "
+
+            if command -v ip >/dev/null; then
+                ip -4 addr show | awk '/inet / {print $2}' | cut -d/ -f1
+            else
+                echo "IP command not found. Please enter IP manually."
+            fi
+                read -p "Enter the IP address to use: " LOCATION
+        ;;
+    
+        4)
+            echo "Option [4] selected"
+            read -p "Enter your latitude and longitude in the form (latitude,longitude): " LOCATION
+        ;;
+
+        5)
+            echo "Option [5] selected"
+            read -p "Enter the name of your city or town: " LOCATION
+        ;;
+
+        *)
+            echo "Invalid selection, install script aborted"
+            exit 1
+        ;;
+
+    esac
+
+    # write as env variables
+    echo "export RAINSHINE_API_KEY=$API_KEY" >> "$HOME/.bashrc"
+    echo "export RAINSHINE_LOCATION=$LOCATION" >> "$HOME/.bashrc"
+}
+
 cd "$(dirname "$0")/src" || exit 1
 
 # compile to an executable and handle errors if compilation fails
@@ -33,56 +89,29 @@ if [[ ":$PATH:" != *":$INSTALL_DIR:"* ]]; then
     echo "$INSTALL_DIR added to PATH"
 fi
 
-# get the user's API key and location
-echo "Please enter your weatherapi.com API key (input is hidden):"
-read -sp "API Key: " API_KEY
-echo -e "\nThe following formats for locations are valid:\n[1] US Zipcode\n[2] Canada Postalcode\n[3] IP Address\n[4] Latitude/Longitude (decimal degree)\n[5] City name (not recommended for small towns or cities with common names)\n"
-read -p "Enter your option [1,2,3,4,5]: " FORMAT_SELECTION
+# check to see if the user already has environment variables from a previous installation
+if [[ $(grep -q "RAINSHINE_API_KEY" ~/.bashrc && grep -q "RAINSHINE_LOCATION" ~/.bashrc; echo $?) -eq 0 ]]; then
 
-# Ask the user for their location based on their selection
-case "$FORMAT_SELECTION" in
+    echo "You appear to already have environment variables for RainShine from a previous installation."
+    read -rp "Use these existing environment variables? [Y/n]: " ANSWER
 
-    1)
-        echo "Option [1] selected"
-        read -p "Enter your US Zipcode: " LOCATION
-    ;;
+    # default answer is yes
+    ANSWER=${ANSWER:-Y}
 
-    2)
-        echo "Option [2] selected"
-        read -p "Enter your Canada Postalcode (do not use spaces): " LOCATION
-    ;;
+    if [[ "$ANSWER" =~ ^[Yy]$ ]]; then
+        echo "Using existing environment variables."
+    else
+        echo "Proceeding without using the existing variables."
+        awk '!/RAINSHINE_API_KEY/ && !/RAINSHINE_LOCATION/' ~/.bashrc > ~/.bashrc.tmp && mv ~/.bashrc.tmp ~/.bashrc # remove old environment variables to prevent clogging ~/.bashrc
+        configEnvVars # configure the environment variables manually
+    fi
 
-    3)
-        echo "Option [3] selected"
-        echo "Available IP addresses: "
-        if command -v ip >/dev/null; then
-            ip -4 addr show | awk '/inet / {print $2}' | cut -d/ -f1
-        else
-            echo "IP command not found. Please enter IP manually."
-        fi
-        read -p "Enter the IP address to use: " LOCATION
-    ;;
-    
-    4)
-        echo "Option [4] selected"
-        read -p "Enter your latitude and longitude in the form (latitude,longitude): " LOCATION
-    ;;
+else 
 
-    5)
-        echo "Option [5] selected"
-        read -p "Enter the name of your city or town: " LOCATION
-    ;;
+    # configure the environment variables manually
+    configEnvVars
 
-    *)
-        echo "Invalid selection, install script aborted"
-        exit 1
-    ;;
-
-esac
-
-# write as env variables
-echo "export RAINSHINE_API_KEY=$API_KEY" >> "$HOME/.bashrc"
-echo "export RAINSHINE_LOCATION=$LOCATION" >> "$HOME/.bashrc"
+fi
 
 # update the shell
 source "$HOME/.bashrc"
